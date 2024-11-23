c561022cd6cb0c42664d063901a57cadb25520ae
Maria Fernanda Romero Alvarado <fmalvarado@uninorte.edu.co>
2024-11-23 00:56:49 -0500

    error manejando cliente
    ja sabroso

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # Configuration Parameters
 CONFIG_PARAMS = {
-    'SERVER_IP_ADDRESS': '192.168.1.91',
-    'SERVER_PORT': 8081,
-    'SERVER_MAX_CLIENTS': 50,
-    'EXIT_MESSAGE': 'exit'
+    'SERVER_IP_ADDRESS': '192.168.1.91',  # Dirección IP del servidor
+    'SERVER_PORT': 8081,                  # Puerto del servidor principal
+    'SERVER_MAX_CLIENTS': 50,             # Máximo de clientes que pueden conectarse al servidor
+    'EXIT_MESSAGE': 'exit',               # Mensaje para cerrar la conexión desde el cliente
+    'WORKER_PORTS': [8082, 8083],         # Puertos para los workers
 }
